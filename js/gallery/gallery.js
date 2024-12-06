9c5abee0b6f9a4b35da9e8ec8dbabdde554edf52
1boombacks1 <boombacks2711@gmail.com>
2024-12-06 18:14:25 +0300

    successed

--- js/gallery/gallery.js
+++ js/gallery/gallery.js
@@ -1,8 +1,11 @@
 import { showFullPictureHandler } from './render_full_picture.js';
 import { getPhotos } from '../server.js';
+import { showFilters } from '../filter.js';
 
 
 const pictures = document.querySelector('.pictures');
 getPhotos().then((photosData) => {
+  showFilters(photosData);
+
   pictures.addEventListener('click', showFullPictureHandler(photosData));
 });
